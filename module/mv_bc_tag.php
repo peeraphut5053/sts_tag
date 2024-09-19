cbc9228dee5796ca295f2ad283ab42953f8b3d47
Pongteedee <pongteedee@gmail.com>
2024-09-19 08:00:28 +0700

    Scan Tag Finishing

--- module/mv_bc_tag.php
+++ module/mv_bc_tag.php
@@ -4,7 +4,8 @@ while (list($key, $data) = each($_GET) OR list($key, $data) = each($_POST)) {
 }
 include "./initial.php";
 
-$sql = "select * from mv_bc_tag where id = '" . $id_tag . "';";
+if ($load == "old_lot") {
+	$sql = "select * from mv_bc_tag where id = '" . $id_tag . "';";
 
 //echo $sql;
 $cSql = new SqlSrv();
@@ -15,4 +16,22 @@ if ($rs[1]) {
 	echo json_encode('false');
 	http_response_code(404);
   }
+}
+
+if ($load == "scan") {
+	$sql = "select id, sts_no, sts_no2,sts_no3, qty_sts_no, qty_sts_no2, qty_sts_no3, qty1, qty2 
+from mv_bc_tag
+where id = '" . $tag_id . "';";
+
+//echo $sql;
+$cSql = new SqlSrv();
+$rs = $cSql->SqlQuery($conn, $sql);
+if ($rs[1]) {
+	echo json_encode($rs[1]);
+  } else {
+	echo json_encode('false');
+	http_response_code(404);
+  }
+}
+
 ?>
